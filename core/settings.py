d466b95d2c25b503c154916c6ed3f7ab76a2ae8e
MasoudRostami <masoudr101@gmail.com>
2026-05-01 23:38:06 +0330

    updated

--- core/settings.py
+++ core/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-&#x7zscx34u&_fagjo4r(a@vndeuf0(hi2)pvcm$z8h3cmhi_l'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = []
 
 
 # Application definition
